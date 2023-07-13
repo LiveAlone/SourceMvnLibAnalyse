364130f008c039fc1de8dd7564586582af05089f
yaoqijun <yaoqijunmail@gmail.com>
2023-07-13 20:37:46 +0800

    ff

--- sp-core/src/main/java/org/yqj/source/spcore/framework/aop/BusinessTraceAspect.java
+++ sp-core/src/main/java/org/yqj/source/spcore/framework/aop/BusinessTraceAspect.java
@@ -25,7 +25,7 @@ public class BusinessTraceAspect {
 //    public PeopleEnabled peopleEnabled;
 
     @Around(value = "execution(* org.yqj.source.spcore.framework.aop.AopUserService.saveUser(..))")
-    public void aroundSaveUser(ProceedingJoinPoint pjp) throws Throwable {
+    public Object aroundSaveUser(ProceedingJoinPoint pjp) throws Throwable {
         log.info("**** around process user before");
         for (Object arg : pjp.getArgs()) {
             log.info("1 args is :{}", arg);
@@ -34,8 +34,9 @@ public class BusinessTraceAspect {
         log.info("3 ProceedingJoinPoint target is :{}", pjp.getTarget());
         log.info("4 ProceedingJoinPoint getSignature is :{}", pjp.getSignature());
         log.info("5 ProceedingJoinPoint string is :{}", pjp.toString());
-        pjp.proceed();
+        Object result = pjp.proceed();
         log.info("**** around process user after");
+        return result;
     }
 
 //    @AfterThrowing(value = "execution(* org.yqj.source.demo.spring.framework.aop.service.AopUserService.validateUser(..))", throwing = "ex")
