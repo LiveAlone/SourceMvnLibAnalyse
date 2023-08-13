334d6ae579c4bb7fd06f3a833c03785f32e76fc6
yaoqijun <yaoqijunmail@gmail.com>
2023-08-13 23:49:32 +0800

    add

--- sp-core/src/main/java/org/yqj/source/spcore/BootDemoApplication.java
+++ sp-core/src/main/java/org/yqj/source/spcore/BootDemoApplication.java
@@ -2,6 +2,7 @@ package org.yqj.source.spcore;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  */
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableCaching
 public class BootDemoApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder()
