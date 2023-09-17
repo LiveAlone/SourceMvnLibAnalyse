71bd9a30dccdd3a8717bac2ed52b3d5f6658949c
yaoqijun <yaoqijunmail@gmail.com>
2023-09-17 21:51:45 +0800

    add

--- cloud-config/src/main/java/org/yqj/source/cloud/config/RunCommandLine.java
+++ cloud-config/src/main/java/org/yqj/source/cloud/config/RunCommandLine.java
@@ -1,8 +1,10 @@
 package org.yqj.source.cloud.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,8 +21,14 @@ public class RunCommandLine implements CommandLineRunner {
     @Value("${spring.application.name:none}")
     private String applicationName;
 
+    @Value("${user.name:none}")
+    private String username;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Override
     public void run(String... args) throws Exception {
-        log.info("application :{} starting", applicationName);
+        log.info("application :{} starting user:{}", applicationName, username);
     }
 }
