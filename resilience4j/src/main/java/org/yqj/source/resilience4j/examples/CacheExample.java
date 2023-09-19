93330c25d54e483261b33f0c2fd9e08b7a5001e6
yaoqijun <yaoqijunmail@gmail.com>
2023-09-20 00:12:06 +0800

    add

---
+++ resilience4j/src/main/java/org/yqj/source/resilience4j/examples/CacheExample.java
@@ -0,0 +1,17 @@
+package org.yqj.source.resilience4j.examples;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Description:
+ *
+ * @author yaoqijun
+ * @date 2023/9/20
+ * Email: yaoqijunmail@foxmail.com
+ */
+@Slf4j
+public class CacheExample {
+    public static void main(String[] args) {
+        // https://resilience4j.readme.io/docs/cache
+    }
+}
