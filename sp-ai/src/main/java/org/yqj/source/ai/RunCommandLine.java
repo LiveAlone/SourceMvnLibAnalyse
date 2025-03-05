fe123341895be463205d7c65815f3ca66b4704fd
LiveAlone <yaoqijunmail@gmail.com>
2025-03-05 19:42:58 +0800

    append key

--- sp-ai/src/main/java/org/yqj/source/ai/RunCommandLine.java
+++ sp-ai/src/main/java/org/yqj/source/ai/RunCommandLine.java
@@ -1,6 +1,12 @@
 package org.yqj.source.ai;
 
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.moonshot.MoonshotChatModel;
+import org.springframework.ai.moonshot.MoonshotChatOptions;
+import org.springframework.ai.moonshot.api.MoonshotApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,8 +20,34 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class RunCommandLine implements CommandLineRunner {
+
+    @Resource
+    private MoonshotChatModel chatModel;
+
     @Override
     public void run(String... args) throws Exception {
-        log.info("starting command line ...");
+        try {
+            // 同步调用返回结果内容
+            ChatResponse response = chatModel.call(
+                    new Prompt(
+                            "列举中国的五个城市名称",
+                            MoonshotChatOptions.builder()
+                                    .model(MoonshotApi.ChatModel.MOONSHOT_V1_8K.getValue())
+                                    .temperature(0.5)
+                                    .build()
+                    ));
+            log.info("ai chat result is response: {}", response.getResult().getOutput().getText());
+
+            // Steam 流式调用, 顺序返回结果内容
+//            chatModel.stream(new Prompt("Generate the names of 5 famous pirates.",
+//                    MoonshotChatOptions.builder()
+//                            .model(MoonshotApi.ChatModel.MOONSHOT_V1_8K.getValue())
+//                            .temperature(0.5).build()
+//            )).subscribe(response -> {
+//                log.info("ai chat result is response: {}", response.getResult().getOutput().getText());
+//            });
+        } catch (Exception e) {
+            log.error("ai chat error is happen", e);
+        }
     }
 }
